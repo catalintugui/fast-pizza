2f8431477c79d2bdd3bca851dc60342dc66282e2
Catalin Tugui <katamaxx23@gmail.com>
2023-10-24 23:33:02 +0300

    adding position to order

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -57,7 +57,9 @@ const userSlice = createSlice({
       })
       .addCase(fetchAddress.rejected, (state, action) => {
         state.status = 'error';
-        state.error = action.error.message;
+        // state.error = action.error.message;
+        state.error =
+          'There was a problem getting your address, please fill this field';
       }),
 });
 
